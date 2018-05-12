6bd66e820fb4b37e94153ca7cec85cbb59fc38fd
Kamil Koczurek <koczurekk@gmail.com>
2018-05-12 15:21:20 +0200

    Documented player.d

--- source/player.d
+++ source/player.d
@@ -3,30 +3,39 @@ module player;
 import std.datetime;
 import std.datetime.stopwatch : StopWatch;
 
+/++ Converts Duration to a number of seconds +/
 float asSeconds(Duration d) {
-  return (cast(float) d.total!"nsecs") / 1000000000;
+  return (cast(float) d.total!"nsecs") / 1_000_000_000;
 }
 
 import dsfml.graphics;
 import dsfml.system;
 
+/++ Class representing the dinosaur +/
 class Player : Drawable {
   private float delta_seconds = 0;
 
-  static immutable float horizontal_offset = 60;
-  static immutable Vector2f size = Vector2f(40, 43);
-  static immutable float initial_jump_speed = 900;
-  static immutable float gravity = 3500;
+  static immutable float horizontal_offset = 60;     /// Distance from the left side of the window at which a player stays
+  static immutable Vector2f size = Vector2f(40, 43); /// Size of the player
+  static immutable float initial_jump_speed = 900;   /// Initial vertical speed immediately after jumping
+  static immutable float gravity = 3500;             /// Gravitational acceleration
+
+  /// Height of the window in which player resides
   float window_height;
 
+  /// Distance that the player has covered in the LAST FRAME
   float displacement() { return delta_seconds * speed; }
 
+  /// Speed at which the player moves to the right
   float speed = 500;
+  /// Distance from the ground
   float height = 0;
+  /// Vertical velocity, positive means up
   float vert_velocity = 0;
 
   private StopWatch _clock;
 
+  /// Default constructor
   this() {
     _clock.start();
   }
@@ -40,11 +49,14 @@ class Player : Drawable {
     target.draw(rt, states);
   }
 
+  /// Attempts to jump
   void jump() {
     if(height == 0) {
       vert_velocity = initial_jump_speed;
     }
   }
+
+  /// Applies gravitational acceleration, moves the player etc.
   void update() {
     delta_seconds = _clock.peek.asSeconds;
 
