82474e57429afa06bc6a6df8514b3ce401945271
Kamil Koczurek <koczurekk@gmail.com>
2018-05-15 12:49:39 +0200

    ResourceManager can create colliders if requested

--- source/resourcemanager.d
+++ source/resourcemanager.d
@@ -1,29 +1,35 @@
 module resourcemanager;
 
+import collider;
 import dsfml.graphics : Texture;
 import dsfml.audio    : SoundBuffer;
 
 private class ResourceManager {
   Texture[string] textures;
+  Collider[string] colliders;
   SoundBuffer[string] buffers;
 
-  bool register(T)(const string path, const string name) if(is(T == Texture)) {
+  bool register(T)(const string path, const string name, bool generate_collider = no_collider) if(is(T == Texture)) {
     auto newtex = new Texture;
-    if(newtex.loadFromFile(path)) {
-      textures[name] = newtex;
-      return true;
+    if(!newtex.loadFromFile(path)) {
+      return false;
     }
 
-    return false;
+    textures[name] = newtex;
+    if(generate_collider) {
+      colliders[path] = new Collider(newtex);
+    }
+
+    return true;
   }
   bool register(T)(const string path, const string name) if(is(T == SoundBuffer)) {
     auto newbuf = new SoundBuffer;
-    if(newbuf.loadFromFile(path)) {
-      buffers[name] = newbuf;
-      return true;
+    if(!newbuf.loadFromFile(path)) {
+      return false;
     }
 
-    return false;
+    buffers[name] = newbuf;
+    return true;
   }
 
   Texture get(T)(const string name) if(is(T == Texture)) {
@@ -33,6 +39,13 @@ private class ResourceManager {
     return buffers[name];
   }
 }
+
+
+/// Named bool to indicate need of a collider
+static bool make_collider = true;
+
+/// Named bool to indicate that no collider is needed
+static bool no_collider = false;
 
 /// Manages sound buffers and textures
 static ResourceManager resource_manager;
