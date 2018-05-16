18c53b6d9c194bba8ea29d4527d4cd3b9b04ebba
Kamil Koczurek <koczurekk@gmail.com>
2018-05-16 19:43:49 +0200

    Rewrote cactus logic

--- source/cactus.d
+++ source/cactus.d
@@ -4,14 +4,8 @@ import dsfml.graphics;
 import resourcemanager;
 import collidable;
 import collider;
-
-private uint asInt(Cactus.Width w) {
-  final switch(w) {
-    case Cactus.Width.Thin:   return 1;
-    case Cactus.Width.Medium: return 2;
-    case Cactus.Width.Wide:   return 3;
-  }
-}
+import std.conv;
+import helpers;
 
 /// Size of a small cactus
 static immutable small_cactus_size = Vector2f(17, 33);
@@ -20,76 +14,57 @@ static immutable medium_cactus_size = Vector2f(25, 45);
 /// Size of a big cactus
 static immutable big_cactus_size = Vector2f(24, 46);
 
+private static immutable cactus_count = 11;
+
+private string path(const uint i) {
+  return "assets/cactuses/" ~ i.to!string ~ ".png";
+}
+private string name(const uint i) {
+  return "cactus" ~ i.to!string;
+}
+
 static this() {
-  assert(resource_manager.register!Texture("assets/cactus1.png", "small_cactus", make_collider));
-  assert(resource_manager.register!Texture("assets/cactus2.png", "medium_cactus", make_collider));
-  assert(resource_manager.register!Texture("assets/cactus3.png", "big_cactus", make_collider));
-}
-
-private Collider getCollider(Cactus.Type type, Cactus.Width width) {
-  final switch(type) {
-    case Cactus.Type.Big:    return resource_manager.get!Collider("big_cactus").replicate(width.asInt);
-    case Cactus.Type.Medium: return resource_manager.get!Collider("medium_cactus").replicate(width.asInt);
-    case Cactus.Type.Small:  return resource_manager.get!Collider("small_cactus").replicate(width.asInt);
+  foreach(i; 1..cactus_count + 1) {
+    assert(resource_manager.register!Texture(path(i), name(i), make_collider));
   }
 }
 
-/// Get size of a cactus of given type
-Vector2f cactus_size(const Cactus c) {
-  Vector2f res;
-  final switch(c.type) {
-    case Cactus.Type.Small:  res = small_cactus_size;  break;
-    case Cactus.Type.Medium: res = medium_cactus_size; break;
-    case Cactus.Type.Big:    res = big_cactus_size;    break;
-  }
-  return res;
+private const(Collider) getCollider(const uint cactus_id) {
+  return resource_manager.get!Collider(cactus_id.name);
+}
+
+private Vector2f cactus_size(const uint cactus_id) {
+  return resource_manager.get!Texture(cactus_id.name).getSize.changeType!float;
 }
 
 /// Cactus class
 class Cactus : Drawable, Collidable {
-	private Collider collider_not_translated;
+	private const(Collider) collider_not_translated;
 
-  /// Represents type of a cactus
-  enum Type {
-    Small, Medium, Big
-  }
-
-  /// Represents width of a cactus
-  enum Width {
-    Thin, Medium, Wide
-  }
+  private uint cactus_id;
 
   /// Distance from the left side of the window
   float horizontal_offset;
-  /// Type of the cactus
-  Type type;
-  /// Width of the cactus
-  Width width;
 
   /// Height of the game window
   float window_height;
 
   /// Constructs a cactus with given type and position
-  this(Type _type, Width _width, float _horizontal_offset) {
-    type = _type;
-    width = _width;
+  this(uint _cactus_id, float _horizontal_offset) {
+    cactus_id = _cactus_id;
     horizontal_offset = _horizontal_offset;
 
-		collider_not_translated = getCollider(type, width);
+		collider_not_translated = getCollider(cactus_id);
 	}
 
   /// Texture of the cactus
   const(Texture) texture() const {
-    final switch(type) {
-      case Type.Small:  return resource_manager.get!Texture("small_cactus");
-      case Type.Medium: return resource_manager.get!Texture("medium_cactus");
-      case Type.Big:    return resource_manager.get!Texture("big_cactus");
-    }
+    return resource_manager.get!Texture(cactus_id.name);
   }
 
   /// Position of the cactus
   Vector2f position() const {
-    return Vector2f(horizontal_offset, window_height - cactus_size(this).y);
+    return Vector2f(horizontal_offset, window_height - cactus_id.cactus_size.y);
   }
 
   /// Moves a cactus by a given distance
@@ -101,15 +76,11 @@ class Cactus : Drawable, Collidable {
   override void draw(RenderTarget window, RenderStates states) const {
     RectangleShape s = new RectangleShape;
     s.position(position);
-    s.size(cactus_size(this));
+    s.size(cactus_size(cactus_id));
     s.setTexture(texture);
     s.fillColor(Color(255, 255, 255));
 
     window.draw(s);
-    foreach(i; 1..width.asInt) {
-      s.move(Vector2f(cactus_size(this).x, 0));
-      window.draw(s);
-    }
 		window.draw(collider_not_translated.translate(position));
   }
 
@@ -122,20 +93,7 @@ class Cactus : Drawable, Collidable {
 Cactus random_cactus_at(float horizontal_offset, uint window_height) {
   import std.random : uniform;
 
-  Cactus.Type t;
-  final switch(uniform!uint % 3) {
-    case 0: t = Cactus.Type.Small;  break;
-    case 1: t = Cactus.Type.Medium; break;
-    case 2: t = Cactus.Type.Big;    break;
-  }
-  Cactus.Width w;
-  final switch(uniform!uint % 3) {
-    case 0: w = Cactus.Width.Thin;   break;
-    case 1: w = Cactus.Width.Medium; break;
-    case 2: w = Cactus.Width.Wide;   break;
-  }
-
-  auto res = new Cactus(t, w, horizontal_offset);
+  auto res = new Cactus(uniform!uint % cactus_count + 1, horizontal_offset);
   res.window_height = window_height;
   return res;
 }
